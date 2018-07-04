1e044f6236cfcd49da0b4893095c1362f0714729
Liviu Ionescu <ilg@livius.net>
2018-07-04 18:08:27 +0300

    try doxygen 1.8.12-1

--- scripts/travis.linux.sh
+++ scripts/travis.linux.sh
@@ -60,7 +60,8 @@ function do_before_install() {
 
   # https://launchpad.net/ubuntu/+source/doxygen
   # Install a newer doxygen from launchpad binaries.
-  doxy_deb=doxygen_1.8.13-10_amd64.deb
+  # https://launchpad.net/ubuntu/+archive/primary/+files/doxygen_1.8.13-10_amd64.deb
+  doxy_deb=doxygen_1.8.12-1_amd64.deb
   mkdir -p ${HOME}/downloads
   do_run curl -L --silent https://launchpad.net/ubuntu/+archive/primary/+files/${doxy_deb} -o ${HOME}/downloads/${doxy_deb}
   do_run sudo dpkg -i ${HOME}/downloads/${doxy_deb}
